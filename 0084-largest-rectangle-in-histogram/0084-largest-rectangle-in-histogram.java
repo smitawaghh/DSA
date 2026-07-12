15a22ec0ae56bbe5fd579bbb3be9d1115d4afa3c
smitawaghh <smitawagh49@gmail.com>
2026-07-12 23:55:54 +0530

    Time: 115 ms (6.07%), Space: 76 MB (96.49%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
@@ -1,51 +1,65 @@
 class Solution {
+
     public int largestRectangleArea(int[] heights) {
-        int n=heights.length;
-        int maximum=0;
-        int[] nse = findnse(heights);
-        int[] pse = findpse(heights);
-        for (int i=0; i<n; i++) {
-            maximum = Math.max(maximum, heights[i]*(nse[i]-pse[i]-1));
+        int n = heights.length;
+
+        int[] pse = findPSE(heights);
+        int[] nse = findNSE(heights);
+
+        int maxArea = 0;
+
+        for (int i = 0; i < n; i++) {
+            int width = nse[i] - pse[i] - 1;
+            int area = heights[i] * width;
+            maxArea = Math.max(maxArea, area);
         }
-        return maximum;
+
+        return maxArea;
     }
 
-       public int[] findnse(int[] heights) {
-        int n=heights.length;
-        int[] nse= new int[n];
-        Stack<Integer> st = new Stack<>();
-        for (int i=n-1; i>=0; i--) {
-            while (!st.isEmpty() && heights[st.peek()] >= heights[i] ) {
-                st.pop();
-            }
-            if (st.isEmpty()) {
-                nse[i] =n;
-            }
-            else {
-                nse[i] = st.peek();
-            }
-            st.push(i);
-        }
-        return nse;
-       }
-      public int[] findpse(int[] heights) {
-        int n=heights.length;
+    public int[] findPSE(int[] heights) {
+        int n = heights.length;
         int[] pse = new int[n];
         Stack<Integer> st = new Stack<>();
-        for (int i=0; i<n; i++) {
+
+        for (int i = 0; i < n; i++) {
+
             while (!st.isEmpty() && heights[st.peek()] >= heights[i]) {
                 st.pop();
             }
+
             if (st.isEmpty()) {
-                pse[i] =-1;
-            }
-            else {
+                pse[i] = -1;
+            } else {
                 pse[i] = st.peek();
             }
+
             st.push(i);
         }
+
         return pse;
-       }
-        
-    
+    }
+
+    public int[] findNSE(int[] heights) {
+        int n = heights.length;
+        int[] nse = new int[n];
+        Stack<Integer> st = new Stack<>();
+
+        for (int i = n - 1; i >= 0; i--) {
+
+            while (!st.isEmpty() && heights[st.peek()] >= heights[i]) {
+                st.pop();
+            }
+
+            if (st.isEmpty()) {
+                nse[i] = n;
+            } else {
+                nse[i] = st.peek();
+            }
+
+            st.push(i);
+        }
+
+        return nse;
+    }
 }
