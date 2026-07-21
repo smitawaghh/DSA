616914aa0d6657fb9137acd043dc7807a9f41946
smitawaghh <smitawagh49@gmail.com>
2026-07-21 23:57:35 +0530

    Time: 0 ms (100%), Space: 43.9 MB (27.76%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -5,23 +5,30 @@ class Solution {
         while (low<=high) {
             int  mid = (low+high)/2;
             if (nums[mid] == target) return mid;
+            //check if left half is sorted
             if (nums[low]<=nums[mid]) {
+                //check if given target lies on left half
                 if (target >= nums[low] && target<=nums[mid]) {
                     high = mid-1;
                 }
+                //if not so, search on right
                 else {
                     low = mid+1;
                 }
             }
+            //right half is sorted
             else {
+                //check if element lies on right half
                 if (target >= nums[mid] && target <= nums[high]) {
                     low = mid+1;
                 }
+                //if not so, check on left
                 else {
                     high = mid-1;
                 }
             }
         }
+        //if not found return -1
         return -1;
     }
 }
