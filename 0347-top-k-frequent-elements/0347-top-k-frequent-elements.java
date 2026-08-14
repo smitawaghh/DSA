c3dc7fde9051ba699ff11246778bfb21a08a30e6
smitawaghh <smitawagh49@gmail.com>
2026-08-14 23:37:35 +0530

    Time: 13 ms (79.89%), Space: 47.4 MB (82.82%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,22 +1,73 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        HashMap<Integer,Integer> map = new HashMap<>();
+        HashMap<Integer, Integer> map = new HashMap<>();
         int[] ans = new int[k];
         for (int num: nums) {
-            map.put(num, map.getOrDefault(num, 0)+1);
+            map.put(num, map.getOrDefault(num,0)+1);
         }
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> a[1]-b[1]);
-        for (Map.Entry<Integer, Integer> e: map.entrySet()) {
-            pq.offer(new int[]{e.getKey(), e.getValue()});
-                    if (pq.size() > k) {
-                    pq.poll();
-        }
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[1] - b[1]);
+        for (Map.Entry<Integer, Integer> e : map.entrySet()) {
 
+            pq.offer(new int[]{e.getKey(), e.getValue()});
+
+            if (pq.size() > k) {
+                pq.poll();
+            }
         }
+        
+        int i = 0;
+
         while (!pq.isEmpty()) {
-          ans[--k] = pq.poll()[0];
+            ans[i] = pq.poll()[0];
+            i++;
         }
 
         return ans;
+
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class Solution {
+//     public int[] topKFrequent(int[] nums, int k) {
+//         HashMap<Integer,Integer> map = new HashMap<>();
+//         int[] ans = new int[k];
+//         for (int num: nums) {
+//             map.put(num, map.getOrDefault(num, 0)+1);
+//         }
+//         PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> a[1]-b[1]);
+//         for (Map.Entry<Integer, Integer> e: map.entrySet()) {
+//             pq.offer(new int[]{e.getKey(), e.getValue()});
+//                     if (pq.size() > k) {
+//                     pq.poll();
+//         }
+
+//         }
+//         while (!pq.isEmpty()) {
+//           ans[--k] = pq.poll()[0];
+//         }
+
+//         return ans;
+//     }
+// }
